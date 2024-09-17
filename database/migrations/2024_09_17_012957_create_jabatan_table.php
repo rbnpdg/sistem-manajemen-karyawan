3f828249f1f7280903526ade4dd70c1210ec7560
rbnpdg <aditamarubenpandega@gmail.com>
2024-09-17 11:42:37 +0700

    up file

---
+++ database/migrations/2024_09_17_012957_create_jabatan_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('jabatan', function (Blueprint $table) {
+            $table->id(); // Default to 'bigIncrements' (unsignedBigInteger)
+            $table->string('nama');
+            $table->decimal('gaji_pokok', 15, 2);
+            $table->decimal('tunjangan', 15, 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('jabatan');
+    }
+};
