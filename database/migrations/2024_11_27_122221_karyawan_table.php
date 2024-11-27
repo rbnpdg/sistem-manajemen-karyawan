9d4f374bad9fe4b9a10b702e9b2a5f6ea710027e
rbnpdg <aditamarubenpandega@gmail.com>
2024-11-27 19:37:56 +0700

    add karyawan migration
    tambahkan filem igrasi untuk tabel karyawan

---
+++ database/migrations/2024_11_27_122221_karyawan_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('karyawan', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('divisi_id');
+            $table->unsignedBigInteger('jabatan_id');
+            $table->string('nama');
+            $table->date('tanggal_lahir');
+            $table->text('alamat');
+            $table->string('no_telepon', 15);
+            $table->date('tanggal_masuk');
+            $table->timestamps();
+
+            //foreign key
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('divisi_id')->references('id')->on('divisi')->onDelete('cascade');
+            $table->foreign('jabatan_id')->references('id')->on('jabatan')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('karyawan');
+    }
+};
